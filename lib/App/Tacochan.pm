4ab9254f241a8cf50a96e5aa558658fedc5d8a41
Takumi Akiyama <t.akiym@gmail.com>
2012-01-08 22:07:26 +0900

    Initial commit

---
+++ lib/App/Tacochan.pm
@@ -0,0 +1,44 @@
+package App::Tacochan;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+tacochan - Skype message delivery by HTTP
+
+=head1 SYNOPSIS
+
+  tacochan
+
+=head1 OPTIONS
+
+=over 4
+
+=item -o, --host
+
+The interface a TCP based server daemon binds to. Defauts to undef,
+which lets most server backends bind the any (*) interface. This
+option doesn't mean anything if the server does not support TCP
+socket.
+
+=item -p, --port (default: 4969)
+
+The port number a TCP based server daemon listens on. Defaults to
+4969. This option doesn't mean anything if the server does not support
+TCP socket.
+
+=item -h, --help
+
+Show help for this command.
+
+=item -v, --version
+
+Show version.
+
+=back
+
+=cut
